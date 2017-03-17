c29c315c2899f2d61347ef78ea0ddd23c194e450
Carlosa215 <carlosagg9519@gmail.com>
2017-03-16 20:07:03 -0700

    se cambiaron a los registros rs,rt y rd. falta la asignacion del registro Imm

--- ALU.asm
+++ ALU.asm
@@ -161,16 +161,18 @@ Op10: db 'Se realiza un bne',0xa
 tamano_Op10: equ $-Op10
 Op11: db 'Se realiza un addu',0xa
 tamano_Op11: equ $-Op11
-Op12: db 'Se realiza un sltiu',0xa
-tamano_Op12: equ $-Op12
+;Op12: db 'Se realiza un sltiu',0xa
+;tamano_Op12: equ $-Op12
 Op13: db 'Se realiza un sltu',0xa
 tamano_Op13: equ $-Op13
 Op14: db 'Se realiza un subu',0xa
 tamano_Op14: equ $-Op14
+Op15: db 'Se realiza un slt',0xa
+tamano_Op15: equ $-Op15
 l3: db 'Fin del Programa!',0xa
 tamano_l3: equ $-l3
 
-num1: equ 0xb
+num1: equ 0xc
 
 ;Definicion de los caracteres especiales para limpiar la pantalla
 limpiar    db 0x1b, "[2J", 0x1b, "[H"
@@ -200,8 +202,8 @@ _start:
 	mov r9,num1 ; registro que indica la operacion a realizar
 	mov r8,0 ; registro indice de operacion
 
-	mov r12,-5; r12 --- registro que almacena el primer parametro
-	mov r13,3; r13 ---  registro que almacena el segundo parametro
+	mov r12, -10; r12 --- registro que almacena el primer parametro
+	mov r13, 3; r13 ---  registro que almacena el segundo parametro
 	;Se compara el registro r9 con el r8 para saber que operacion se desea realizar
 
 	cmp r8,r9
@@ -239,119 +241,172 @@ _start:
 	inc r8
 	cmp r8,r9
 	je _addu ; 11 La ALU debe realizar un addu
-	;inc r8
-	;cmp r8,r9
-	;je _sltiu ; 12 La ALU debe realizar un sltiu
-	;inc r8
-	;cmp r8,r9
-	;je _sltu ; 13 La ALU debe realizar un sltu
-	;inc r8
-	;cmp r8,r9
-	;je _subu ; 14 La ALU debe realizar un subu
+	inc r8
+	cmp r8,r9
+	je _sltiu ; 12 La ALU debe realizar un sltiu
+	inc r8
+	cmp r8,r9
+	je _sltu ; 13 La ALU debe realizar un sltu
+	inc r8
+	cmp r8,r9
+	je _subu ; 14 La ALU debe realizar un subu
+	inc r8
+	cmp r8,r9
+	je _slt ; 15 La ALU debe realizar un slt
+
 
 	;Direcciones de operacion de instrucciones
 
-	_add:
+	_add:   ;-------------listo con registros MIPS para utilizar------------------------------
+
 	impr_texto Op1,tamano_Op1 ; Indica al usuario que operacion se realiza
 
-	mov rax,r12 ;Se pasan los datos a los registros que van a operar
-	mov rbx,r13
+	mov rax,r12 ; [rsp+r14] ; $rs Se pasan los datos a los registros que van a operar
+	mov rbx,r13 ; [rsp+r13] ; $rt
 
 	add rax,rbx ; Se realiza la operacion
+	mov [rsp+r12],rax; $rd se guarda la operacion en el registro deseado
 	_AddR:
 	cmp r8,r9 ; terminada la operacion, se sale del programa
 	jae _end
+;-------------------------------------------------------------------------
 
+;----------------Listo con Registros MIPS para utilizar--------------------
 	_addu:
 	impr_texto Op11,tamano_Op11 ; Indica al usuario que operacion se realiza
 
-	mov rax,r12 ;Se pasan los datos a los registros que van a operar
-	mov rbx,r13
-	_compare:
-	cmp r12,0 ; compara r12 con 0 para saber si es mayor a el
-	jg _CR2 ;si es mayor a cero realiza un salto, ya que no se debe modificar el registro
-	mov rax,r12 ; se pasa r12 al registro rax, el cual es propio de la operacion imul
-	mov r12,-1 ; se carga el registro con -1 para multiplicar
-	imul  r12; se multiplica por -1 para que el resultado sea positivo
-	mov r12,rax ; guarda el valor modificado en el registro deseado
-	_CR2: ; brinco a evaluar el contenido del segundo registro
-	cmp r13,0 ; procedimiento igual al aplicado al registro anterior
-	jg _CRL ; brinco a ejecucion ALU
-	mov rax,r13
-	mov r13,-1
-	imul r13
-	mov r13,rax
-	_CRL: ; Ejecucion ALU realiza la operacion deseada
-		add r12,r13 ; Se realiza la operacion
+	mov rax,r13 ; [rsp+r13] ;
+
+	_compare11:
+	cmp rax,0 ; compara r12 con 0 para saber si es mayor a el
+	jg _CR211 ;si es mayor a cero realiza un salto, ya que no se debe modificar el registro
+	mov rbx,-1 ; se carga el registro con -1 para multiplicar
+	imul  rbx; se multiplica por -1 para que el resultado sea positivo
+	mov rbx,rax ; guarda el valor modificado en el registro deseado
+	_CR211: ; brinco a evaluar el contenido del segundo registro
+	mov rax , r12 ; [rsp+ r14]
+	cmp rax,0 ; procedimiento igual al aplicado al registro anterior
+	jg _CRL11 ; brinco a ejecucion ALU
+	mov rbp,-1
+	imul rbp
+	_CRL11: ; Ejecucion ALU realiza la operacion deseada
+
+	add rax,rbx ; Se realiza la operacion
+	mov [rsp+r12],rax; $rd se guarda la operacion en el registro deseado
+
 	_AdduR:
 	cmp r8,r9 ; terminada la operacion, se sale del programa
 	jae _end
+;------------------------------------------------------------------------------
 
+;-----------------Listo para usar con registros MIPS ---------------------------
 	_and:
 	impr_texto Op2,tamano_Op2
-	mov rax,r12
-	mov rbx,r13
+	mov rax,r12 ; [rsp+r14]
+	mov rbx,r13 ; [rsp+r13]
 
 	and rax,rbx
+	mov [rsp+r12], rax
 	_AndR:
 	cmp r8,r9
 	jae _end
 
+;----------------Listo Con Registros MIPS a Utilizar -----------------------
 	_or:
 	impr_texto Op3,tamano_Op3
-	mov rax,r12
-	mov rbx,r13
+	mov rax,r12 ; [rsp+r14]
+	mov rbx,r13 ; [rsp+r13]
 
 	or rax,rbx
+	mov [rsp+r12], rax
 	_OrR:
 	cmp r8,r9
 	jae _end
+;--------------------------------------------------------------------------
 
+;------------listo Con Registros MIPS a Utilizar---------------------------
 	_nor:
 	impr_texto Op4,tamano_Op4
-	mov rax,r12
-	mov rbx,r13
+	mov rax,r12 ; [rsp+r14]
+	mov rbx,r13	; [rsp+r13]
 
 	or rax,rbx
 	not rax
+	mov [rsp+r12], rax
 	_NorR:
 	cmp r8,r9
 	jae _end
+;--------------------------------------------------------------------------
 
+;------------FALTA ASIGNACION DE SHAMT---------------------------------------
 	_shl:
 	impr_texto Op5,tamano_Op5
-	mov rax,r12
-	mov rcx,r13
+	mov rax,[rsp+r13]
+	mov rcx,r13 ;
 
 	shl rax,cl
+	mov [rsp+r12], rax
 	_ShlR:
 	cmp r8,r9
 	jae _end
+;--------------------------------------------------------------------------
 
+;------------FALTA ASIGNACION DE SHAMT-------------------------------------
 	_shr:
 	impr_texto Op6,tamano_Op6
-	mov rax,r12
-	mov rcx,r13
+	mov rax, [rsp+13]
+	mov rcx,r13	;
 
 	shr rax,cl
+	mov [rsp+r12], rax
 	_ShrR:
 	cmp r8,r9
 	jae _end
+;--------------------------------------------------------------------------
 
+;--------------Registros MIPS Listos a Operar------------------------------
 	_sub:
 	impr_texto Op7,tamano_Op7
-	mov rax,r12
-	mov rbx,r13
+	mov rax,[rsp+r14]
+	mov rbx,[rsp+r13]
 
 	sub rax,rbx
+	mov [rsp+r12], rax
 	_SubR:
 	cmp r8,r9
 	jae _end
+;---------------------------------------------------------------------------
+;-----------------Con Registros MIPS Listos a Operar------------------------
+	_subu:
+	impr_texto Op14,tamano_Op14
 
-	_imul:
+	mov rax,[rsp+r13] ;
+
+	_compare14:
+	cmp rax,0 ; compara r12 con 0 para saber si es mayor a el
+	jg _CR211 ;si es mayor a cero realiza un salto, ya que no se debe modificar el registro
+	mov rbx,-1 ; se carga el registro con -1 para multiplicar
+	imul  rbx; se multiplica por -1 para que el resultado sea positivo
+	mov rbx,rax ; guarda el valor modificado en el registro deseado
+	_CR214: ; brinco a evaluar el contenido del segundo registro
+	mov rax , [rsp+ r14]
+	cmp rax,0 ; procedimiento igual al aplicado al registro anterior
+	jg _CRL14 ; brinco a ejecucion ALU
+	mov rbp,-1
+	imul rbp
+	_CRL14: ; Ejecucion ALU realiza la operacion deseada
+
+	sub rax,rbx
+
+	_SubuR:
+	cmp r8,r9
+	jae _end
+
+	_imul: ;el resultado se guarda en rax, y si sobre pasa el tamano de este registro
+				 ; la bandera CF y OF se ponen en uno y los demas bits se guardan en RDX
 	impr_texto Op8,tamano_Op8
-	mov rax,r12
-	mov rbx,r13
+	mov rax,[rsp+r14]
+	mov rbx,[rsp+r13]
 	imul rbx
 
 	_ImulR:
@@ -360,8 +415,8 @@ _start:
 
 	_beq:
 	impr_texto Op9,tamano_Op9
-	mov rax, r12
-	mov rbx, r13
+	mov rax, [rsp+r14]
+	mov rbx, [rsp+r9]
 	sub rax,rbx
 	cmp rax,0
 	je _BeqC
@@ -370,13 +425,14 @@ _start:
 	_BeqC:
 	mov rax,1
 	_BeqR:
+	mov [rsp+r13], rax
 	cmp r8,r9
 	jae _end
 
 	_bne:
 	impr_texto Op10,tamano_Op10
-	mov rax, r12
-	mov rbx, r13
+	mov rax, [rsp+r14]
+	mov rbx, [rsp+r9]
 	sub rax,rbx
 	cmp rax,0
 	je _BneC
@@ -385,8 +441,89 @@ _start:
 	_BneC:
 	mov rax,0
 	_BneR:
+	mov [rsp+r13], rax
 	cmp r8,r9
 	jae _end
+
+	_slt:
+	impr_texto Op15,tamano_Op15
+	mov rax, [rsp+r14]
+	mov rbx, [rsp+r13]
+	_SltRR:
+	cmp rax,rbx
+	jl _Runo
+	mov rax,0
+	cmp rax,0
+	je _SltR
+	_Runo:
+	mov rax,1
+	_SltR:
+	mov [rsp+r12], rax
+	cmp r8,r9
+	jae _end
+;--------------------------REVISAR ESTA OPERACION------------------------------
+	_sltu:
+	impr_texto Op13,tamano_Op13
+
+	mov rax,[rsp+r13] ;
+	_compare13:
+	cmp rax,0 ; compara r12 con 0 para saber si es mayor a el
+	jg _CR213 ;si es mayor a cero realiza un salto, ya que no se debe modificar el registro
+	mov rbx,-1 ; se carga el registro con -1 para multiplicar
+	imul  rbx; se multiplica por -1 para que el resultado sea positivo
+	mov rbx,rax ; guarda el valor modificado en el registro deseado
+	_CR213: ; brinco a evaluar el contenido del segundo registro
+	mov rax , [rsp+ r14]
+	cmp rax,0 ; procedimiento igual al aplicado al registro anterior
+	jg _CRL13 ; brinco a ejecucion ALU
+	mov rbp,-1
+	imul rbp
+	_CRL13: ; Ejecucion ALU realiza la operacion deseada
+
+	_SltuRR:
+	cmp rax,rbx
+	jl _Runo
+	mov rax,0
+	cmp rax,0
+	je _SltuR
+	_Runo:
+	mov rax,1
+	_SltuR:
+	cmp r8,r9
+	jae _end
+
+	_sltui:
+	impr_texto Op13,tamano_Op13
+	mov rax, r12
+	mov rbx, r13
+
+	_compare15:
+	cmp r12,0 ; compara r12 con 0 para saber si es mayor a el
+	jg _CR215 ;si es mayor a cero realiza un salto, ya que no se debe modificar el registro
+	mov rax,r12 ; se pasa r12 al registro rax, el cual es propio de la operacion imul
+	mov r12,-1 ; se carga el registro con -1 para multiplicar
+	imul  r12; se multiplica por -1 para que el resultado sea positivo
+	mov r12,rax ; guarda el valor modificado en el registro deseado
+	_CR215: ; brinco a evaluar el contenido del segundo registro
+	cmp r13,0 ; procedimiento igual al aplicado al registro anterior
+	jg _CRL15 ; brinco a ejecucion ALU
+	mov rax,r13
+	mov r13,-1
+	imul r13
+	mov r13,rax
+	_CRL15: ; Ejecucion ALU realiza la operacion deseada
+
+	cmp r12,r13
+	jl _Runo15
+	mov r12,0
+	cmp r12,0
+	je _SltuiR
+	_Runo15:
+	mov r12,1
+	_SltuiR:
+	cmp r8,r9
+	jae _end
+
 
 	_end:
 	impr_texto l3,tamano_l3
